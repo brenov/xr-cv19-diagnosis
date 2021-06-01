81a8569e4f7645d0251b841847cdc98bc8fc39f2
Breno Viana <bmfviana@gmail.com>
2021-06-01 16:12:49 -0300

    changes tab space to 4

--- augmentation.py
+++ augmentation.py
@@ -1,91 +1,95 @@
 """This file holds algorithms for data augmentation of images."""
 
+import numpy as np
+
 
 def adjust_contrast(img):
-  """
-  Function to adjust the contrast of the given image.
+    """
+    Function to adjust the contrast of the given image.
 
-  Parameters
-  ----------
-  img : ndarray
-    Input image data.
+    Parameters
+    ----------
+    img : ndarray
+        Input image data.
 
-  Returns
-  -------
-  out : ndarray
-    Contrast adjusted version of the input image data.
-  """
-  # TODO
-  pass
+    Returns
+    -------
+    out : ndarray
+        Contrast adjusted version of the input image data.
+    """
+    # TODO
+    pass
 
 
 def adjust_sharpness(img):
-  """
-  Function to adjust the sharpness of the given image.
+    """
+    Function to adjust the sharpness of the given image.
 
-  Parameters
-  ----------
-  img : ndarray
-    Input image data.
+    Parameters
+    ----------
+    img : ndarray
+        Input image data.
 
-  Returns
-  -------
-  out : ndarray
-    Sharpness adjusted version of the input image data.
-  """
-  # TODO
-  pass
+    Returns
+    -------
+    out : ndarray
+        Sharpness adjusted version of the input image data.
+    """
+    # TODO
+    pass
 
 
-def add_noise(img):
-  """
-  Function to adjust the sharpness of the given image.
+def add_noise(img, mean, std):
+    """
+    Function to adjust the sharpness of the given image.
 
-  Parameters
-  ----------
-  img : ndarray
-    Input image data.
+    Parameters
+    ----------
+    img : ndarray
+        Input image data.
+    mean : float
+        AA.
 
-  Returns
-  -------
-  out : ndarray
-    Noisy version of the input image data.
-  """
-  # TODO
-  pass
+    Returns
+    -------
+    out : ndarray
+        Noisy version of the input image data.
+    """
+    # TODO
+    pass
 
 
 def clockwise_rotation(img):
-  """
-  Function to rotate the given image 45 degrees clockwise.
+    """
+    Function to rotate the given image 45 degrees clockwise.
 
-  Parameters
-  ----------
-  img : ndarray
-    Input image data.
+    Parameters
+    ----------
+    img : ndarray
+        Input image data.
 
-  Returns
-  -------
-  out : ndarray
-    45 degrees rotated version of the input image data.
-  """
-  # TODO
-  pass
+    Returns
+    -------
+    out : ndarray
+        45 degrees rotated version of the input image data.
+    """
+    # TODO
+    pass
 
 
 def anticlockwise_rotation(img):
-  """
-  Function to rotate the given image 45 degrees anticlockwise.
+    """
+    Function to rotate the given image 45 degrees anticlockwise.
 
-  Parameters
-  ----------
-  img : ndarray
-    Input image data.
+    Parameters
+    ----------
+    img : ndarray
+        Input image data.
 
-  Returns
-  -------
-  out : ndarray
-    -45 degrees rotated version of the input image data.
-  """
-  # TODO
-  pass
+    Returns
+    -------
+    out : ndarray
+        -45 degrees rotated version of the input image data.
+    """
+    # TODO
+    pass
